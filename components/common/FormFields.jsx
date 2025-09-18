8b0e1822405dd89dcedba6df62476ef0c9175e7c
Shahid Hasan Rumon <shahidhasanruman3@gmail.com>
2025-09-18 20:48:41 +0600

    Contact page designed

--- components/common/FormFields.jsx
+++ components/common/FormFields.jsx
@@ -1,3 +1,8 @@
+'use client';
+
+import { ArrowDown } from '@/public/assets/icons/icons';
+import { useState } from 'react';
+
 const inputStyles =
   'w-full bg-transparent placeholder-text-secondary text-text pe-4 py-3.5 ring-1 focus:ring-2 ring-border hover:ring-primary focus:ring-primary rounded-[10px] outline-none transition-colors duration-300';
 
@@ -22,18 +27,27 @@ export const Input = ({
   ...props
 }) => {
   return (
-    <div className={`w-full relative ${label && 'flex flex-col gap-2 md:gap-4'}`}>
+    <div className={`w-full h-max relative ${label && 'flex flex-col gap-2 md:gap-4'}`}>
       {label && <Label label={label} htmlFor={name || id} />}
-      {Icon && <Icon className='size-6 text-text-secondary absolute left-4 bottom-3.5' />}
+      {Icon && (
+        <Icon
+          className={`size-6 text-text-secondary absolute left-4 ${
+            error && helperText ? 'bottom-[50px]' : 'bottom-3.5'
+          }`}
+        />
+      )}
       <input
         type={type}
         name={name}
         id={id}
         placeholder={placeholder}
-        className={`${inputStyles} ${Icon ? 'ps-[52px]' : 'ps-4'} ${className}`}
+        className={`${inputStyles} ${error && helperText && '!ring-red-500'} ${
+          Icon ? 'ps-[52px]' : 'ps-4'
+        } ${className}`}
         {...props}
       />
-      {error && helperText && <p className='text-red-500 text-sm mt-1 ml-1'>{helperText}</p>}
+      {/* Error Message */}
+      {error && helperText && <p className='text-red-500 text-sm ml-1'>{helperText}</p>}
     </div>
   );
 };
@@ -57,36 +71,73 @@ export const Textarea = ({
         id={id}
         placeholder={placeholder}
         rows={rows}
-        className={`ps-4 resize-none ${inputStyles} ${className}`}
+        className={`ps-4 resize-none ${inputStyles} ${error && helperText && '!ring-red-500'} ${className}`}
         {...props}
       />
-      {error && helperText && <p className='text-red-500 text-sm mt-1 ml-1'>{helperText}</p>}
+      {/* Error Message */}
+      {error && helperText && <p className='text-red-500 text-sm ml-1'>{helperText}</p>}
     </div>
   );
 };
 
-export const Select = ({ options, name = '', id = '', defaultValue = '', className = '', ...props }) => {
+export const Select = ({
+  options,
+  children,
+  label,
+  name = '',
+  id = '',
+  defaultValue = '',
+  className = '',
+  optionClassName = '',
+  error,
+  helperText,
+  ...props
+}) => {
   return (
-    <select
-      name={name}
-      id={id}
-      defaultValue={defaultValue}
-      className={`${inputStyles} ${className}`}
-      {...props}
-    ></select>
+    <div className={`w-full h-max relative ${label && 'flex flex-col gap-2 md:gap-4'}`}>
+      {label && <Label label={label} htmlFor={name || id} />}
+      <select
+        name={name}
+        id={id}
+        defaultValue={defaultValue}
+        className={`ps-4 !pe-12 appearance-none ${inputStyles} ${error && helperText && '!ring-red-500'} ${className}`}
+        {...props}
+      >
+        {children}
+        {options &&
+          options.map((opt, idx) => {
+            const { value, text, disabled, ...prop } = opt;
+
+            return (
+              <option
+                key={idx}
+                value={value}
+                disabled={disabled}
+                {...prop}
+                className={`text-text bg-bg ${optionClassName}`}
+              >
+                {text}
+              </option>
+            );
+          })}
+      </select>
+      <ArrowDown className={`size-4 absolute right-4 ${error && helperText ? 'bottom-[54px]' : 'bottom-[18px]'}`} />
+      {/* Error Message */}
+      {error && helperText && <p className='text-red-500 text-sm ml-1'>{helperText}</p>}
+    </div>
   );
 };
 
 export const Checkbox = ({ name = '', id = '', className = '', ...props }) => {
-  let checked = false;
+  const [checked, setChecked] = useState(false);
 
   return (
     <input
       type='checkbox'
       name={name}
       id={id}
-      onChange={e => (checked = e.target.checked)}
-      className={`w-5 min-w-5 h-5 accent-primary appearance-none border border-[#DDE0E6] rounded-md relative cursor-pointer checked:border-primary checked:bg-primary focus:outline-none after:content-[""] after:absolute after:top-0.5 after:left-1.5 after:border-b-2 after:border-r-2 after:h-3 after:w-1.5 after:rotate-45 ${
+      onChange={e => setChecked(e.target.checked)}
+      className={`w-5 min-w-5 h-5 bg-bg accent-primary appearance-none border border-border rounded-md relative cursor-pointer checked:border-primary checked:bg-primary focus:outline-none after:content-[""] after:absolute after:top-0.5 after:left-1.5 after:border-b-2 after:border-r-2 after:h-3 after:w-1.5 after:rotate-45 ${
         checked ? 'after:border-white' : 'after:border-transparent'
       } ${className}`}
       {...props}
--- views/contact/ContactForm.jsx
+++ views/contact/ContactForm.jsx
@@ -2,9 +2,49 @@
 
 import { useState } from 'react';
 
-import { Input, Textarea } from '@/components/common/FormFields';
+import { Checkbox, Input, Select, Textarea } from '@/components/common/FormFields';
 import { Call, Message, User } from '@/public/assets/icons/icons';
 import Button from '@/components/Button';
+
+const companyNameOptions = [
+  {
+    value: '',
+    text: 'Company Name',
+    disabled: true
+  },
+  {
+    value: 'company-1',
+    text: 'Company 1'
+  },
+  {
+    value: 'company-2',
+    text: 'Company 2'
+  },
+  {
+    value: 'company-3',
+    text: 'Company 3'
+  }
+];
+
+const roleOptions = [
+  {
+    value: '',
+    text: 'Your Role',
+    disabled: true
+  },
+  {
+    value: 'role-1',
+    text: 'Role 1'
+  },
+  {
+    value: 'role-2',
+    text: 'Role 2'
+  },
+  {
+    value: 'role-3',
+    text: 'Role 3'
+  }
+];
 
 const ContactForm = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -26,29 +66,33 @@ const ContactForm = () => {
         <Input label='Email' name='email' id='email' type='email' placeholder='Email' Icon={Message} />
       </div>
 
+      {/* Company Name & Role */}
+      <div className={inputGroupsContainerStyle}>
+        <Select
+          label='Company Name'
+          options={companyNameOptions}
+          name='company_name'
+          id='company_name'
+          defaultValue=''
+        />
+        <Select label='Your Role' options={roleOptions} name='your_role' id='your_role' defaultValue='' />
+      </div>
+
       {/* Message */}
       <Textarea label='Message' id='message' name='message' placeholder='Type here' />
 
+      {/* Checkbox for Privacy Policy */}
+      <div className='flex items-center gap-3'>
+        <Checkbox />
+        <span className='text-text text-sm'>
+          I agree to the Privacy Policy and to be contacted by MSC regarding my query.
+        </span>
+      </div>
+
+      {/* Submit */}
       <Button type='submit' size='sm' variant='contained' disabled={isLoading}>
         {isLoading ? 'Sending...' : 'Send Message'}
       </Button>
-
-      {/* Success/Error Message */}
-      {/* {submitMessage && (
-          <Alert
-            severity={submitMessage.type}
-            sx={{
-              mt: 2,
-              borderRadius: 2.5,
-              '& .MuiAlert-message': {
-                fontSize: 14
-              }
-            }}
-            onClose={() => setSubmitMessage(null)}
-          >
-            {submitMessage.message}
-          </Alert>
-        )} */}
     </form>
   );
 };
